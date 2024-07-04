9c7cde0a832fb6e8359a321cc1165594bc87ab46
Lucas Blom <51747171+SkateFastEatTrash@users.noreply.github.com>
2024-07-04 10:47:41 +0200

    chore: Update budget settings page with budget edit functionality

--- lib/pages/budget_settings.dart
+++ lib/pages/budget_settings.dart
@@ -1,67 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_budget_application/pages/budget_overview.dart';
+import 'package:flutter_budget_application/providers/budget_provider.dart';
+import 'package:provider/provider.dart';
 
-class BudgetSettings extends StatefulWidget {
-  const BudgetSettings({super.key});
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _BudgetSettingsState createState() => _BudgetSettingsState();
-}
-
-class _BudgetSettingsState extends State<BudgetSettings> {
+class BudgetSettings extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+    var budget = Provider.of<BudgetProvider>(context).budget;
     return Scaffold(
+
       appBar: AppBar(
         title: Align(
           alignment: Alignment.center,
           child: Row(
-            children: <Widget>[
-              const Padding(padding: EdgeInsets.only(right: 5)),
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
               FloatingActionButton.small(
                 backgroundColor: Colors.white,
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => BudgetOverview()));
+                  // ignore: avoid_types_as_parameter_names, non_constant_identifier_names
+                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext) => BudgetSettings())); 
                 },
-                child: const Icon(Icons.arrow_back_ios_new_rounded),
+                child: const Icon(Icons.settings),
               ),
-              const SizedBox(width: 15),
-              const Text('Settings', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            ],
+              const Text('Budget Settings', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              FloatingActionButton.small(
+                backgroundColor: Colors.white,
+                onPressed: () {
+                  // ignore: avoid_types_as_parameter_names, non_constant_identifier_names
+                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext) => ExpenseOverviewPage())); 
+                },
+                child: const Icon(Icons.arrow_forward_ios_rounded),
+              ),],
           ),
-        )
+        ) 
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          FractionallySizedBox(
-            alignment: Alignment.center,
-            widthFactor: 0.9, 
-            child: Column(              
-              children: <Widget>[
-                const Text('Set your budget for the month', style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
-                const SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    floatingLabelAlignment: FloatingLabelAlignment.center,
-                    labelText: 'Budget',
-                    labelStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment. center,
+            children: [
+              Column(
+                children: [
+                  const Text("Budget:", style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal)),
+                  const SizedBox(height: 5),
+                  Text(budget.toString(), style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  FloatingActionButton.small(
+                    backgroundColor: Colors.white,
+                    onPressed: () {
+                      Provider.of<BudgetProvider>(context, listen: false).resetBudget();
+                    },
+                    child: const Icon(Icons.refresh),
                   ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    // Save the budget
-                  },
-                  child: const Text('Save'),
-                ),
-              ]
-            ),
+                ],
+              )
+            ],
           ),
+          _BudgetEdit(),
         ],
       ),
+    );
+  }
+}
+
+class _BudgetEdit extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BudgetEditState();
+}
+
+class _BudgetEditState extends State<_BudgetEdit> {
+  final _budgetController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _budgetController,
+            decoration: const InputDecoration(labelText: "Amount"),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Provider.of<BudgetProvider>(context, listen: false).setBudget(
+              double.parse(_budgetController.text),
+            );
+            _budgetController.clear();
+          },
+          child: const Text("Add"),
+        ),
+      ],
     );
   }
 }
